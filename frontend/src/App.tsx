7c423d5587c23723c196f17ddbf0e7003efacd92
soumi-ahaan <soumi.k@ahaansoftwareconsulting.com>
2026-07-27 12:51:41 +0530

    scrollTop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import './App.css'
 import { AllRoutes } from './routes/AllRoutes'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+
 function App() {
 
   return (
    <>
+    <ScrollToTop />
    <AllRoutes/>
    </>
   )
