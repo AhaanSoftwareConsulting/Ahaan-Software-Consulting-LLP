4c59e5795f1ca0391ee2124c02c369c835d56495
cyber-s-tech <subhadeep.ahaansoftware@gmail.com>
2026-08-05 20:29:58 +0530

    change2

--- frontend/src/components/layouts/header/Header.tsx
+++ frontend/src/components/layouts/header/Header.tsx
@@ -143,7 +143,7 @@ export const Header = () => {
 
                 <a
                   href="tel:+16465759575"
-                  className="flex h-8 w-8 sm:h-10 sm:w-10 items-center justify-center rounded-full bg-gradient-to-r from-[#C48A18] to-[#E6B33C] text-black shadow-lg transition hover:scale-105"
+                  className="flex h-8 w-8 sm:h-10 sm:w-10  items-center justify-center rounded-full bg-gradient-to-r from-[#C48A18] to-[#E6B33C] text-black shadow-lg transition hover:scale-105"
                 >
                   <PhoneOutgoingIcon size={18} weight="light" />
                 </a>
