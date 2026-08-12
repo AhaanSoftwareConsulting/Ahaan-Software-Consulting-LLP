a3ffe072245f24bbf8a3012941ab603fc7cff0f0
soumi-ahaan <soumi.k@ahaansoftwareconsulting.com>
2026-08-12 18:23:28 +0530

    svg fixed now

--- frontend/src/components/pages/casestudy/components/CaseStudySolutionsSection.tsx
+++ frontend/src/components/pages/casestudy/components/CaseStudySolutionsSection.tsx
@@ -30,28 +30,31 @@ export const CaseStudySolutionsSection = ({
    *
    * It should never cross the text/image content.
    */
-  // const snakePath = `
+  //  const snakePath = `
   //   M 220,20
 
-  //   C 800,-70 1020,260 960,570
+  //   C 520,-40 820,-30 930,150
+  //   C 1010,280 1000,470 900,590
 
-  //   C 920,730 800,800 500,800
+  //   C 820,680 700,700 520,700
+  //   C 350,700 190,700 105,800
 
-  //   C 200,800 40,850 40,1050
+  //   C 15,895 15,1110 135,1180
+  //   C 190,1220 300,1220 800,1220
 
-  //   C 40,1250 200,1320 500,1320
+  //   C 900,1280 700,1220 900,1270
+  //   C 985,1390 985,1540 900,1640
 
-  //   C 800,1320 960,1390 960,1590
+  //   C 820,1730 690,1760 510,1760
+  //   C 340,1760 190,1770 105,1870
 
-  //   C 60,1790 800,1860 500,1860
+  //   C 35,1950 35,2120 110,2190
+  //   C 200,2295 460,2245 820,2255
 
-  //   C 200,1860 40,1920 40,2120
-
-  //   C 40,2320 200,2380 500,2380
+  //   C 680,2225 790,2240 850,2310
   // `;
 
-
-  const snakePath = `
+const snakePath = `
     M 220,20
 
     C 520,-40 820,-30 930,150
@@ -60,8 +63,8 @@ export const CaseStudySolutionsSection = ({
     C 820,680 700,700 520,700
     C 350,700 190,700 105,800
 
-    C 15,895 15,1110 135,1180
-    C 590,1280 460,1150 820,1290
+    C 35,885 35,1010 105,1080
+    C 190,1150 350,1150 520,1150
 
     C 700,1150 830,1170 900,1270
     C 985,1390 985,1540 900,1640
@@ -69,12 +72,11 @@ export const CaseStudySolutionsSection = ({
     C 820,1730 690,1760 510,1760
     C 340,1760 190,1770 105,1870
 
-    C 35,1950 35,2120 110,2190
-    C 200,2295 460,2245 820,2255
+    C 35,1950 35,2070 110,2140
+    C 200,2225 350,2225 520,2225
 
     C 680,2225 790,2240 850,2310
   `;
-
 
   return (
     <section
@@ -162,8 +164,7 @@ export const CaseStudySolutionsSection = ({
             className="
               relative
               z-10
-              flex
-              flex-col
+              grid grid-cols-1
               gap-32
               md:gap-40
               xl:gap-52
@@ -193,14 +194,11 @@ export const CaseStudySolutionsSection = ({
                   }}
                   className={`
                     relative
-                    flex
-                    flex-col
+                    grid grid-cols-1
                     items-center
-                    gap-8
-                    md:flex-row
-                    md:gap-12
-                    xl:gap-20
-                    ${isOdd ? "md:flex-row-reverse" : ""}
+                  
+                    gap-8 md:gap-12 xl:gap-20
+                    md:grid-cols-2
                   `}
                 >
 
@@ -208,7 +206,10 @@ export const CaseStudySolutionsSection = ({
                   {/* IMAGE SECTION */}
                   {/* ================================================= */}
 
-                  <div className="relative z-20 w-full md:w-1/2">
+                  <div className={`
+    relative z-20 justify-center items-center
+    ${isOdd ? "md:col-start-2" : "md:col-start-1"}
+  `}>
 
                     {solution.title && (
                       <h3
@@ -218,6 +219,7 @@ export const CaseStudySolutionsSection = ({
                           mb-4
                           flex
                           items-center
+                          
                           gap-2
                           text-xl
                           font-extrabold
@@ -243,7 +245,7 @@ export const CaseStudySolutionsSection = ({
                         className="
                           relative
                           z-20
-                          inline-block
+                          inline-block w-fit lg:w-md
                           overflow-hidden
                           rounded-2xl
                           border
@@ -259,10 +261,10 @@ export const CaseStudySolutionsSection = ({
                         <img
                           src={solution.image}
                           alt={solution.title || "Solution"}
-                          className="
+                          className=" relative
                             max-h-[300px]
                             w-full
-                            max-w-full
+                            max-w-full 
                             rounded-xl
                             object-contain
                             md:max-h-[320px]
@@ -278,23 +280,23 @@ export const CaseStudySolutionsSection = ({
                   {/* ================================================= */}
 
                   <div
-                    className="
+                    className={`
                       relative
                       z-30
                       w-full
                       rounded-2xl
                       bg-white
-                      p-4
-                      md:w-1/2
+                      p-4                      
                       md:p-6
-                    "
+                      ${isOdd ? "md:col-start-1 md:row-start-1" : "md:col-start-2"}
+                    `}
                   >
                     {solution.description && (
                       <div
                         className="
                           relative
                           z-30
-                          text-base
+                          text-base lg:text-center
                           leading-relaxed
                           text-[#555555]
                           md:text-lg
