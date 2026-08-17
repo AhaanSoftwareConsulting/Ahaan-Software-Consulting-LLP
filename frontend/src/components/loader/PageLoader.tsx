c09abf5efe5062459fa059e7663f7456310732a5
Sugam-Dev-25 <sugamkarmakar25@gmail.com>
2026-08-17 18:23:24 +0530

    change background of page loader

--- frontend/src/components/loader/PageLoader.tsx
+++ frontend/src/components/loader/PageLoader.tsx
@@ -1,19 +1,20 @@
 export const PageLoader = () => {
   return (
-    <div className="fixed inset-0 z-[9999] flex min-h-screen items-center justify-center bg-[#0B0B0B]">
+    <div className="fixed inset-0 z-[9999] flex min-h-screen items-center justify-center bg-white/60 backdrop-blur-lg">
       <div className="flex flex-col items-center gap-5">
         
         {/* Loader */}
         <div className="relative h-16 w-16">
-          <div className="absolute inset-0 rounded-full border-4 border-[#C9A227]/20" />
+          <div className="absolute inset-0 rounded-full border-4 border-[#161616]/20" />
 
-          <div className="absolute inset-0 animate-spin rounded-full border-4 border-transparent border-t-[#C9A227]" />
+          <div className="absolute inset-0 animate-spin rounded-full border-4 border-transparent border-t-[#161616]" />
         </div>
 
         {/* Text */}
-        <p className="text-sm font-medium tracking-[0.3em] text-[#C9A227]">
+        <p className="text-sm font-medium tracking-[0.3em] text-[#161616]">
           LOADING
         </p>
+
       </div>
     </div>
   );
